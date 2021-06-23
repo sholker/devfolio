adcbb0f92a3f7a78495734ea48b9f73b0c2ef974
oris <oris1002@gmail.com>
2021-06-23 14:18:59 +0300

    add my code

---
+++ db.php
@@ -0,0 +1,110 @@
+<?php
+
+function db_connection(){
+    //create a mySQL DB connection:
+    $dbhost = "182.50.133.173";
+    $dbuser = "studDB21a";
+    $dbpass = "stud21DB1!";
+    $dbname = "studDB21a";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// testing connection success
+    if (mysqli_connect_errno()) {
+        die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+    }
+    return $connection;
+}
+function get_projects()
+{
+////create a mySQL DB connection:
+//    $dbhost = "182.50.133.173";
+//    $dbuser = "studDB21a";
+//    $dbpass = "stud21DB1!";
+//    $dbname = "studDB21a";
+//    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+//
+//// testing connection success
+//    if (mysqli_connect_errno()) {
+//        die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+//    }
+
+
+//$query = "insert into tbl_prods(name,img_url,cat_id) values ('$prodName','$prodImg','$catId')";
+
+    $connection = db_connection();
+    $query = "SELECT * FROM `tbl_test_90`";
+//    echo $query;
+    $result = $mysqli -> query;
+    $result = mysqli_query($connection, $query);
+
+    if (!$result) {
+        die("DB query failed.");
+    }
+
+    $row = mysqli_fetch_assoc($result); // there is only 1 item with id=X
+
+    $result = $connection->query($query);
+    $i=1;
+    if ($result->num_rows > 0) {
+// output data of each row
+
+
+        while ($row = $result->fetch_assoc()) {
+            $format = 'popup%d';
+            $id= sprintf($format, $i);
+            echo  '<a href="#' . $id . '" class="project col-md-4">' . $row['Name'] . '<img src="img/work-2.jpg" alt="" class="img-fluid"></a>' . '<div id="' . $id . '" class="popup">' . '<a href="#" class="close">&times;</a>' . '<h2>' . $row["Name"] . '</h2>' . '<p> About: <br>' . $row['Description'] . ' <br><a href="' . $row['url'] . '">click here to project</a></p></div><a href="#" class="close-popup"></a>';
+
+                $i+=1;
+
+//            echo "<p>  - Name: " . $row["Name"] . " " . $row["Desciption"] . "</p><br>";
+        }
+    }
+//    mysqli_close();
+}
+
+function get_experience()
+{
+    //create a mySQL DB connection:
+    $dbhost = "182.50.133.173";
+    $dbuser = "studDB21a";
+    $dbpass = "stud21DB1!";
+    $dbname = "studDB21a";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// testing connection success
+    if (mysqli_connect_errno()) {
+        die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+    }
+
+//$query = "insert into tbl_prods(name,img_url,cat_id) values ('$prodName','$prodImg','$catId')";
+
+    $connection = db_connection();
+    $query = "SELECT * FROM  tbl_user_90_experience";
+//    echo $query;
+    $result = $mysqli -> query;
+    $result = mysqli_query($connection, $query);
+
+    if (!$result) {
+        die("DB query failed.");
+    }
+
+    $row = mysqli_fetch_assoc($result); // there is only 1 item with id=X
+
+    $result = $connection->query($query);
+    $i=1;
+    if ($result->num_rows > 0) {
+// output data of each row
+
+
+        while ($row = $result->fetch_assoc()) {
+            $format = 'popup%d';
+            $id= sprintf($format, $i);
+
+            echo '<div class="col-md-4"><div class="service-box"><div class="service-ico"></div><div class="service-content"><h2 class="s-title">' . $row["company"] . '</h2><i class="text-center">' . $row['years'] . '</i><br>
+                        <p class="s-description left_text">' . $row["description"] . '</p></div></div> </div>';
+        }
+    }
+    mysqli_close();
+}
+
+?>
